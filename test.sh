6a5423c3687332a9ff7a5837dafed3293b5c71a3
SachaGor <sachagorecki254@gmail.com>
2023-01-17 22:36:07 +0100

    v0.1.2

---
+++ test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp pages/appartements/appart1.html pages/appartements/appart2.html
+cp pages/appartements/appart1.html pages/appartements/appart3.html
+cp pages/appartements/appart1.html pages/appartements/appart4.html
+cp pages/appartements/appart1.html pages/appartements/appart5.html
+cp pages/appartements/appart1.html pages/appartements/appart6.html
+cp pages/appartements/appart1.html pages/appartements/appart7.html
+cp pages/appartements/appart1.html pages/appartements/appart8.html
+cp pages/appartements/appart1.html pages/appartements/appart9.html
